0ab522290dc7557bbbab0838e82c5ab7aeb4cbe7
studynon <ljcmy1@gmail.com>
2017-05-23 16:40:20 +0800

    change some decode codes

--- seq2seq_attention_decode.py
+++ seq2seq_attention_decode.py
@@ -153,7 +153,7 @@ class BSDecoder(object):
     # while step < FLAGS.max_decode_steps: #
     time.sleep(DECODE_LOOP_DELAY_SECS)
     if not self._Decode(self._saver, sess):
-      continue
+      return
     print("decode step: {} ".format(step))
 
 
